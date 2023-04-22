b204e7c1e7b5051b7e9f8f1014ba24d1d7c5ae62
Kiryu Sakakibara <kiryusaka@gmail.com>
2023-04-22 16:38:32 -0400

    Player shoots to the right

--- src/gameobjects/ShipSprite.ts
+++ src/gameobjects/ShipSprite.ts
@@ -1,9 +1,26 @@
+import Bullet from "./Bullet"
+
+type Key = Phaser.Input.Keyboard.Key
+var KeyCodes = Phaser.Input.Keyboard.KeyCodes
+var Vec2 = Phaser.Math.Vector2
+
 export default class ShipSprite extends Phaser.Physics.Arcade.Sprite {
-    /** The cursor input keys for the scene this ship belongs to */
-    cursors: Phaser.Types.Input.Keyboard.CursorKeys
+    /** The input keys for the scene*/
+    inputs : {
+        up: Key
+        down: Key,
+        left: Key,
+        right: Key,
+        shoot: Key
+    }
     /** The speed of the ship */
-    speed = 400
-    coolDown = 100
+    speed = 500
+    /** The max cooldown of the ship */
+    maxCooldown = 40
+    /** The remaining shooting cooldown of the ship */
+    cooldown = 0
+    /** The collection of bullets owned by the ship */
+    bullets: Phaser.GameObjects.Group
 
     constructor(scene: Phaser.Scene, x: number, y: number, texture: string | Phaser.Textures.Texture) {
         // Create the gameobject and add it to the physics world
@@ -13,7 +30,16 @@ export default class ShipSprite extends Phaser.Physics.Arcade.Sprite {
         this.setCollideWorldBounds(true)
 
         // Create the inputs
-        this.cursors = scene.input.keyboard.createCursorKeys()
+        let keyboard = scene.input.keyboard
+        this.inputs = {...this.inputs, ...keyboard.createCursorKeys()}
+        this.inputs.shoot = keyboard.addKey(KeyCodes.SPACE)
+        
+        // Create the bullets
+        this.bullets = scene.add.group({
+            classType: Bullet,
+            maxSize: 200,
+            runChildUpdate: true
+        })
     }
 
     protected preUpdate(time: number, delta: number): void {
@@ -24,25 +50,41 @@ export default class ShipSprite extends Phaser.Physics.Arcade.Sprite {
     update(time: number, delta: number): void {
         //super.update(args)
 
-        this.coolDown -= delta
+        this.cooldown -= delta
 
-        // Ship velocity
+        this.handleMovement()
+        this.handleShoot()
+    }
+
+    handleMovement() {
         let velocity = new Phaser.Math.Vector2
-        if (this.cursors.left.isDown && this.cursors.right.isUp) {
+        if (this.inputs.left.isDown && this.inputs.right.isUp) {
             velocity.x = -1
-        } else if (this.cursors.left.isUp && this.cursors.right.isDown) {
+        } else if (this.inputs.left.isUp && this.inputs.right.isDown) {
             velocity.x = 1
         } else {
             velocity.x = 0
         }
-        if (this.cursors.up.isDown && this.cursors.down.isUp) {
+        if (this.inputs.up.isDown && this.inputs.down.isUp) {
             velocity.y = -1
-        } else if (this.cursors.up.isUp && this.cursors.down.isDown) {
+        } else if (this.inputs.up.isUp && this.inputs.down.isDown) {
             velocity.y = 1
         } else {
             velocity.y = 0
         }
         velocity.normalize().scale(this.speed)
         this.setVelocity(velocity.x, velocity.y)
+    }
+
+    handleShoot() {
+        if (this.inputs.shoot.isDown && this.cooldown <= 0) {
+            const bullet: Bullet = this.bullets.get()
+            if (bullet) {
+               const yOffset = Math.random()*30-15
+               bullet.spawn(this.body.center.x + this.displayWidth/3, this.body.center.y + yOffset, 0, 800, 0)
+               bullet.setDepth(-1)
+               this.cooldown = this.maxCooldown
+            }
+        } 
     }
 }
--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -23,8 +23,8 @@ export default class GameScene extends Phaser.Scene {
 
     create() {
         //this.physics.world.setBounds(0, 0, 800, 600)
-        this.walpurgisNacht = this.add.sprite(400, 100, "walpurgisnachtImage")
-        this.shipSprite = new ShipSprite(this, 400, 600, "walpurgisnachtImage")
+        this.walpurgisNacht = this.add.sprite(700, 300, "walpurgisnachtImage")
+        this.shipSprite = new ShipSprite(this, 100, 300, "walpurgisnachtImage")
         this.shipSprite.setScale(0.2, 0.2)
         this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
 
@@ -38,32 +38,7 @@ export default class GameScene extends Phaser.Scene {
     
     update(time: number, delta: number): void {
         this.shipSprite.update(time, delta)
-        if (this.spacebar.isDown && this.shipSprite.coolDown <= 0) {
-            //this.spawnBullets()
-            const bullet: Bullet = this.bullets.get()
-            if (bullet) {
-                const angle = Phaser.Math.FloatBetween(0, Phaser.Math.PI2)
-                const vel = new Vec2(Math.cos(angle), Math.sin(angle)).scale(500)
-                console.log(vel.length())
-                bullet.spawn(400, 300, angle, vel.x, vel.y)
-                this.shipSprite.coolDown = 10
-            }
-        }
-        //console.log(this.bulletCount, 1000/delta)
     }
-
-    spawnBullets() {
-        for (let i=0; i<5; i++) {
-            const bullet = this.physics.add.sprite(400, 300, "bullet").setScale(0.25, 0.25)
-            bullet.setCollideWorldBounds(true, 1, 1)
-            bullet.setCircle(16)
-            const vel = new Vec2(Phaser.Math.FloatBetween(-1, 1), Phaser.Math.FloatBetween(-1, 1)).normalize().scale(300)
-
-            bullet.setVelocity(vel.x, vel.y)
-            this.bulletCount += 1
-        }
-    }
-
     
     
 }
