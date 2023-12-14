c72a04269ce77648f6abb5bf980094c666d23af2
Kiryu Sakakibara <kiryusaka@gmail.com>
2023-12-14 00:28:40 -0500

    Fixed player bullet hitbox

--- src/gameobjects/Bullets/PlayerBullets/PlayerLinearBullet.ts
+++ src/gameobjects/Bullets/PlayerBullets/PlayerLinearBullet.ts
@@ -2,12 +2,15 @@ import Phaser from "phaser";
 import PlayerBullet from "./PlayerBullet";
 import GameScene from "../../../scenes/GameScene";
 import * as Planck from "planck"
-import { Bits, createBoxFixture, Masks } from "../../PhysicsConstants";
+import { Bits, createBoxFixture, createCircleFixture, Masks } from "../../PhysicsConstants";
 
 export default class PlayerLinearBullet extends PlayerBullet {
     constructor(scene: GameScene, x: number, y: number) {
         super(scene, x, y, "CircleBulletBlue")
-        this.pbody.createFixture(createBoxFixture(
-            11, 6, Bits.playerBullet, Masks.playerBullet))
+        //this.pbody.createFixture(createBoxFixture(
+        //    11, 6, Bits.playerBullet, Masks.playerBullet))
+        this.pbody.createFixture(createCircleFixture(
+            4, Bits.playerBullet, Masks.playerBullet
+        ))
     }
 }
